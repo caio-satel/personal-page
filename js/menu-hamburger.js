83565613bfff2565b217d0edd11596a217b0fae5
Caio Satel <caio_satel@hotmail.com>
2025-01-15 14:24:36 -0300

    feat: nova estruturação da pagina, estilização da nova pagina, delete arquivo js-menu, alterando tipografia

--- js/menu-hamburger.js
+++
@@ -1,5 +0,0 @@
-const menuHamburger = document.querySelector(".hamburger-menu");
-const menuNav = document.querySelector(".header-menu");
-
-menuHamburger.addEventListener("click", () =>
-menuNav.classList.toggle("active"));
